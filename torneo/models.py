7b61f473455dbc8c310159c3975e4584cb15c52c
Katy_Dlc <97708615+KatyDlc@users.noreply.github.com>
2024-04-05 17:24:47 -0400

    Evento + models

--- torneo/models.py
+++ torneo/models.py
@@ -3,4 +3,13 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=200)
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    fecha = models.DateField()
+    juego = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.titulo
     
